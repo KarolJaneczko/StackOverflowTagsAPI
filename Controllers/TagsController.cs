0c2c8d3e1d839d247038434cf68c44d16ad0b786
Karol Janeczko <karol.janeczko@outlook.com>
2024-03-31 13:29:52 +0200

    Pierwsza wrzutka ze szkieletem projektu API

---
+++ Controllers/TagsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackOverflowTagsAPI.Models.StackOverflow;
+
+namespace StackOverflowTagsAPI.Controllers {
+    [ApiController]
+    [Route("Tags")]
+    public class TagsController(ILogger<TagsController> logger) : ControllerBase {
+        private readonly ILogger<TagsController> Logger = logger;
+
+        [HttpGet]
+        [Route("GetTags")]
+        public IEnumerable<Tag> GetTags() {
+            return new List<Tag>();
+        }
+    }
+}
---
+++ Models/StackOverflow/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StackOverflowTagsAPI.Models.StackOverflow {
+    public class Tag {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("has_synonyms")]
+        public bool HasSynonyms { get; set; }
+
+        [JsonPropertyName("is_moderator_only")]
+        public bool IsModeratorOnly { get; set; }
+
+        [JsonPropertyName("is_required")]
+        public bool IsRequired { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public int UserId { get; set; }
+    }
+}
